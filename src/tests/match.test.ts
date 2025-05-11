dad0a3f9ed0859ab844fd55925c6037db66a8aac
rmmir <radumiron12@gmail.com>
2025-05-11 20:53:15 +0300

    test: add todo unit tests for team and match entities

---
+++ src/tests/match.test.ts
@@ -0,0 +1,17 @@
+describe('Match', () => {
+    it.todo('should initialize a match between two teams with correct names and default initializers');
+
+    it.todo('should throw an error if a match with the same team for home and away');
+
+    it.todo('should throw an error if a match with the same name for home and away teams');
+
+    it.todo('should throw an error if trying to start an already started match');
+
+    it.todo('should update the score when the match is in progress');
+
+    it.todo('should throw an error if trying to update the score when the match is not in progress');
+
+    it.todo('should finish the match');
+
+    it.todo('should throw an error if trying to finish a match that is not in progress');
+});
---
+++ src/tests/team.test.ts
@@ -0,0 +1,5 @@
+describe('Team', () => {
+    it.todo('should initialize a team');
+
+    it.todo('should throw an error if the team name is empty');
+});
