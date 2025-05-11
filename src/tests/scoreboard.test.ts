6203afc3905adbd2a1b3602ad95493e37818032c
rmmir <radumiron12@gmail.com>
2025-05-11 22:00:14 +0300

    test: add todo unit tests for scoreboard entity

---
+++ src/tests/scoreboard.test.ts
@@ -0,0 +1,22 @@
+describe('Scoreboard', () => {
+    it.todo('should start a match and add it to the list of matches');
+
+    it.todo('should not allow starting a match with the same teams already in progress');
+
+    it.todo('should update the score of an existing match');
+
+    it.todo('should throw an error when updating the score of a non-existent match');
+
+    it.todo('should finish a match and remove it from the scoreboard');
+
+    it.todo('should throw an error when finishing a match that does not exist');
+
+    it.todo('should return an empty summary when no matches are in progress');
+
+    it.todo('should return matches sorted by total score descending and then by most recent start time');
+
+    it.todo('should handle multiple matches with same total score and sort by most recent');
+    
+    it.todo('should throw an error when trying to update a match that has already been finished');
+  });
+  
